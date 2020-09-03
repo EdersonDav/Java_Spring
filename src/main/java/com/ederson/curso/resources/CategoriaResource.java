466322bc40c5026d0c1b404a584daa0aec5fe0f6
EdersonDav <silva.edersonqueiroz@gmail.com>
2020-09-03 15:00:59 -0300

    List all categories with DTO, using new class and map in resource

--- src/main/java/com/ederson/curso/resources/CategoriaResource.java
+++ src/main/java/com/ederson/curso/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.ederson.curso.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ederson.curso.domain.Categoria;
+import com.ederson.curso.dto.CategoriaDTO;
 import com.ederson.curso.services.CategoriaService;
 
 @RestController
@@ -46,6 +49,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity <List<CategoriaDTO>> findAll() {
+		List<Categoria> objList = service.findAll();
+		List<CategoriaDTO> listDTO = objList.stream().map(dto -> new CategoriaDTO(dto)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 }

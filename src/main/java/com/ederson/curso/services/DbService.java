dccb0dcd183329ee4044d9efb0551b38cf2f7754
EdersonDav <silva.edersonqueiroz@gmail.com>
2020-09-16 12:49:54 -0300

    Add password and crypt

--- src/main/java/com/ederson/curso/services/DbService.java
+++ src/main/java/com/ederson/curso/services/DbService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.ederson.curso.domain.Categoria;
@@ -51,6 +52,8 @@ public class DbService {
 	private PagamentoRepository pagamentoRepository;
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	public void instantiateTestDataBase() throws ParseException{
 		Categoria cat1 = new Categoria(null, "Informática");
@@ -112,7 +115,7 @@ public class DbService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Ederson Silva", "ederson88847103@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Ederson Silva", "ederson88847103@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, bCryptPasswordEncoder.encode("edede") );
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		

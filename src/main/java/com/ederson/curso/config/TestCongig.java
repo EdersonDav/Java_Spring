8c534e7fd92515dc9853f82ee87834972bcf22c5
EdersonDav <silva.edersonqueiroz@gmail.com>
2020-09-16 12:19:46 -0300

    config initial spring security

--- src/main/java/com/ederson/curso/config/TestCongig.java
+++ src/main/java/com/ederson/curso/config/TestCongig.java
@@ -3,6 +3,7 @@ package com.ederson.curso.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
